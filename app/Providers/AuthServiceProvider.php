0c359a371c98ab3b378cca3fb5ca7d6e92369add
Ukulele <358674029@qq.com>
2017-12-05 16:09:04 +0000

    Add user statuses

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Gate;
+use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -13,8 +13,9 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
-        \App\Models\User::class  => \App\Policies\UserPolicy::class,
+      'App\Model' => 'App\Policies\ModelPolicy',
+      \App\Models\User::class  => \App\Policies\UserPolicy::class,
+      \App\Models\Status::class  => \App\Policies\StatusPolicy::class,
     ];
 
     /**
@@ -22,10 +23,10 @@ class AuthServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
-        $this->registerPolicies();
+     public function boot(GateContract $gate)
+     {
+         $this->registerPolicies($gate);
 
-        //
-    }
+         //
+     }
 }
