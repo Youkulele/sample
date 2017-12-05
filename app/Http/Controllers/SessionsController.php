1817910802fe88210b3a0ee00e52254c0265b846
Ukulele <358674029@qq.com>
2017-12-05 13:35:36 +0000

    Add account activations

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -30,15 +30,17 @@ class SessionsController extends Controller
 
        if (Auth::attempt($credentials, $request->has('remember'))) {
            // 登录成功后的相关操作
-           session()->flash('success', '欢迎回来！');
-           return redirect()->intended(route('users.show', [Auth::user()]));
+           if(Auth::user()->activated) {
+             session()->flash('success', '欢迎回来！');
+             return redirect()->intended(route('users.show', [Auth::user()]));
        } else {
            // 登录失败后的相关操作
            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
            return redirect()->back();
        }
        return;
-    }
+     }
+   }
 
     public function destroy()
     {
