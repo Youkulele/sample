d7bcad3c5bab18ff87e2201dca844845b15d4813
Ukulele <358674029@qq.com>
2017-12-06 09:22:34 +0000

    home edit

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -21,12 +21,12 @@
     </div>
   @else
     <div class="jumbotron">
-      <h1>Hello Laravel</h1>
+      <h1>Hello MyFriend</h1>
       <p class="lead">
-        你现在所看到的是 <a href="https://laravel-china.org/laravel-tutorial/5.1">Laravel 入门教程</a> 的项目主页。
+        Welcome!
       </p>
       <p>
-        一切，将从这里开始。
+        你现在所看到的是主页。
       </p>
       <p>
         <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
