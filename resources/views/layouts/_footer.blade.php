fcf121bd5e19647cc813f729a76ec39ca7efa531
Ukulele <358674029@qq.com>
2017-12-06 09:27:49 +0000

    little change1

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -2,8 +2,8 @@
   <footer class="footer">
     <small class="slogon">
       <img class="brand-icon" src="https://dn-phphub.qbox.me/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34">
-      <a href="https://yousails.com/">
-        优帆远扬 | 创造不息，交付不止
+      <a href="http://sheltered-badlands-27967.herokuapp.com/">
+        好名字可以让你的朋友更容易记住你
       </a>
     </small>
     <nav>
